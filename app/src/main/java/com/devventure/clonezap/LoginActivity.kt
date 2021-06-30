a2c970b59d2d8df8321c29705dcbbd253f7f9a77
Lucas Sampaio <lukaz.sampaio@gmail.com>
2021-06-30 15:23:05 -0300

    usar viewbinding ao invvés de FindViewById() em LoginActivity.kt
    redimensionar imagem splashscreen para tamanho 200x200

--- app/src/main/java/com/devventure/clonezap/LoginActivity.kt
+++ app/src/main/java/com/devventure/clonezap/LoginActivity.kt
@@ -2,28 +2,30 @@ package com.devventure.clonezap
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.devventure.clonezap.databinding.ActivityLoginBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private val RC_SIGN_IN = 123
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
-        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             startActivityForResult(
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
